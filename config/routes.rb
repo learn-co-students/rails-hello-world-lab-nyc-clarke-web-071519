1ddf2912710f337f7e2e048909213f1dd115dad2
Varun Kumar <varunkumaranalysis@gmail.com>
2019-08-09 15:46:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #The priority is based upon order of creation: first created == highest priority
+  #See how all your routes lay out with rake routes
+
+  #you can have the root of your site routed with "root"
+  #root 'welcome#index'
+
+  get '/hello_world' => 'hw#hello_world'
+  
+#   #Example of regular route:
+#     get 'products/:id' => 'catalog#view'
+
+#   #Example of named route that can be invoked with purchase_url(id: product.id)
+#     get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+#   #Example resource route (maps HTTP verbs to controller actions automatically):
+#     resources :products
+#   #Example resource route with options:
+#     resources :products do 
+#       member do
+#         get 'short'
+#         post 'toggle'
+#       end
+
+#       collection do
+#         get 'sold'
+#       end
+#     end
+#   #Example resource route with sub-resources:
+#     resources :products do
+#       resources :comments, :sales
+#       resource :seller
+#     end
+#   #Example resource route with more complex sub-resources:
+#     resources :products do
+#       resources :comments
+#       resources :sales do
+#         get 'recent', on: :collection
+#       end
+#     end
 end
